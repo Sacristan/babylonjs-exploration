1bc48c1584966b1bd803853ab27f3a63f45c217e
Sacristan <girts.kesteris@gmail.com>
2018-01-17 17:36:50 +0200

    got project working

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import Game from './Game'
+
+window.addEventListener('DOMContentLoaded', () => {
+  // Create the game using the 'renderCanvas'
+  let game = new Game('renderCanvas')
+
+  // Create the scene
+  game.createScene()
+
+  // start animation
+  game.doRender()
+})
+
+export const x = 'data'
