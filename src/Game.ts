9cf5202294dafe0810acbf5496c4a220cd1ef5dd
Sacristan <girts.kesteris@gmail.com>
2018-01-18 18:33:29 +0200

    add key animation

--- src/Game.ts
+++ src/Game.ts
@@ -22,7 +22,7 @@ class Game {
 
     //Near / Far Clipping Planes
     this._camera.minZ = 0.3
-    this._camera.maxZ = 100
+    this._camera.maxZ = 8
 
     this._camera.setTarget(BABYLON.Vector3.Zero())
     this._camera.attachControl(this._canvas, false)
@@ -33,27 +33,65 @@ class Game {
       this._scene,
       scene => {
         scene.executeWhenReady(function() {
-          console.log('Loading Done')
+          var key: BABYLON.Mesh = new BABYLON.Mesh('dummyMesh')
+
+          var keyFound: boolean = false
 
           for (let mesh of scene.meshes) {
+            // console.log(mesh.name)
+
+            if (!keyFound && mesh.name == 'key') {
+              key = mesh as BABYLON.Mesh
+              keyFound = true
+            }
+
             if (mesh.material) {
               let mat: BABYLON.StandardMaterial = mesh.material as BABYLON.StandardMaterial
               mat.emissiveColor = new BABYLON.Color3(1, 1, 1)
 
-              console.log(mat.name)
+              // console.log(mat.name)
             }
+          }
+
+          if (keyFound) {
+            const fps: number = 30
+            const timeInSeconds: number = 5
+            const totalFrames = 60
+
+            var animation = new BABYLON.Animation(
+              'keyAnimation',
+              'rotation.z',
+              fps,
+              BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+              BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
+            )
+
+            var easingFunction = new BABYLON.SineEase()
+            easingFunction.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT)
+            animation.setEasingFunction(easingFunction)
+
+            var keys = []
+
+            keys.push({
+              frame: 0,
+              value: 0
+            })
+
+            keys.push({
+              frame: totalFrames,
+              value: 2 * Math.PI
+            })
+
+            animation.setKeys(keys)
+
+            key.animations = []
+            key.animations.push(animation)
+
+            scene.beginAnimation(key, 0, totalFrames, true)
           }
         })
       }
     )
-
-    // let sphere = BABYLON.MeshBuilder.CreateSphere(
-    //   'sphere1',
-    //   { segments: 16, diameter: 0.1 },
-    //   this._scene
-    // )
-
-    // sphere.position = new Vector3(-1, 0, 0)
   }
 
   doRender(): void {
