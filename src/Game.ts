6fd87d1aaf960e816e1a4858e4e5c7fbb0a1ca70
Sacristan <girts.kesteris@gmail.com>
2018-01-19 11:20:33 +0200

    animation static lib

--- src/Game.ts
+++ src/Game.ts
@@ -1,11 +1,61 @@
-import * as BABYLON from 'babylonjs'
-import { Vector3, Mesh } from 'babylonjs'
+import * as BABYLON from 'babylonjs' //remove
+import { Vector3, Mesh, Scene } from 'babylonjs'
+
+const animate = (
+  mesh: Mesh,
+  scene: Scene,
+  time: number,
+  propertyName: string,
+  endFramePropertyValue: number
+) => {
+  const fps: number = 30
+  const totalFrames = fps * time
+
+  var animation = new BABYLON.Animation(
+    mesh + 'Animation',
+    propertyName,
+    fps,
+    BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
+  )
+
+  var easingFunction = new BABYLON.SineEase()
+  easingFunction.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT)
+  animation.setEasingFunction(easingFunction)
+
+  var keys = []
+
+  keys.push({
+    frame: 0,
+    value: 0
+  })
+
+  keys.push({
+    frame: totalFrames,
+    value: endFramePropertyValue
+  })
+
+  animation.setKeys(keys)
+
+  mesh.animations = []
+  mesh.animations.push(animation)
+
+  scene.beginAnimation(mesh, 0, totalFrames, false)
+}
+
+const animateDoor = (door: Mesh, scene: Scene): void => {
+  animate(door, scene, 3, 'rotation.y', -155 * 0.0174533)
+}
+
+const animateKey = (key: Mesh, scene: Scene): void => {
+  animate(key, scene, 2, 'rotation.z', Math.PI * 2)
+}
 
 class Game {
   private _canvas: HTMLCanvasElement
   private _engine: BABYLON.Engine
   private _scene: BABYLON.Scene
-  private _camera: BABYLON.FreeCamera
+  private _camera: BABYLON.Camera
   private _light: BABYLON.Light
 
   constructor(canvasElement: string) {
@@ -15,16 +65,20 @@ class Game {
   }
 
   createScene(): void {
+    this._scene = new BABYLON.Scene(this._engine)
+
     var camPos: BABYLON.Vector3 = new BABYLON.Vector3(0, 0.5, 1.5)
 
-    this._scene = new BABYLON.Scene(this._engine)
-    this._camera = new BABYLON.FreeCamera('camera1', camPos, this._scene)
+    let freeCam: BABYLON.FreeCamera = new BABYLON.FreeCamera('camera1', camPos, this._scene)
+    freeCam.setTarget(BABYLON.Vector3.Zero())
+    this._camera = freeCam as BABYLON.Camera
+
+    // this._camera = new BABYLON.ArcRotateCamera("arcCamera", 1, 2, 30, Vector3.Zero(), this._scene)
 
     //Near / Far Clipping Planes
     this._camera.minZ = 0.3
     this._camera.maxZ = 8
 
-    this._camera.setTarget(BABYLON.Vector3.Zero())
     this._camera.attachControl(this._canvas, false)
 
     BABYLON.SceneLoader.Append(
@@ -34,61 +88,30 @@ class Game {
       scene => {
         scene.executeWhenReady(function() {
           var key: BABYLON.Mesh = new BABYLON.Mesh('dummyMesh')
-
-          var keyFound: boolean = false
+          var door: BABYLON.Mesh = new BABYLON.Mesh('dummyMesh')
 
           for (let mesh of scene.meshes) {
-            // console.log(mesh.name)
-
-            if (!keyFound && mesh.name == 'key') {
-              key = mesh as BABYLON.Mesh
-              keyFound = true
+            switch (mesh.name) {
+              case 'key':
+                key = mesh as BABYLON.Mesh
+                break
+              case 'door_right':
+                door = mesh as BABYLON.Mesh
+                break
             }
 
             if (mesh.material) {
               let mat: BABYLON.StandardMaterial = mesh.material as BABYLON.StandardMaterial
               mat.emissiveColor = new BABYLON.Color3(1, 1, 1)
-
-              // console.log(mat.name)
             }
           }
 
-          if (keyFound) {
-            const fps: number = 30
-            const timeInSeconds: number = 5
-            const totalFrames = 60
+          setTimeout(() => {
+            animateKey(key, scene)
+          }, 1000)
 
-            var animation = new BABYLON.Animation(
-              'keyAnimation',
-              'rotation.z',
-              fps,
-              BABYLON.Animation.ANIMATIONTYPE_FLOAT,
-              BABYLON.Animation.ANIMATIONLOOPMODE_CONSTANT
-            )
-
-            var easingFunction = new BABYLON.SineEase()
-            easingFunction.setEasingMode(BABYLON.EasingFunction.EASINGMODE_EASEINOUT)
-            animation.setEasingFunction(easingFunction)
-
-            var keys = []
-
-            keys.push({
-              frame: 0,
-              value: 0
-            })
-
-            keys.push({
-              frame: totalFrames,
-              value: 2 * Math.PI
-            })
-
-            animation.setKeys(keys)
-
-            key.animations = []
-            key.animations.push(animation)
-
-            scene.beginAnimation(key, 0, totalFrames, true)
-          }
+          // animateKey(key, scene)
+          // animateDoor(door, scene)
         })
       }
     )
